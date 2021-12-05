bfa2e751556ee6773b131c5638280238cc37cc76
ianfrossi <ianfrossi@gmail.com>
2021-12-05 15:17:13 -0300

    updated typewriter effect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Typer = function (element, targets, delta) {
     this.element = element;
-    this.currentElement = 0;
+    this.index = 0;
     this.targets = targets;
     this.delta = parseInt(delta, 10);
     this.current = '';
@@ -11,23 +11,24 @@ var Typer = function (element, targets, delta) {
 Typer.prototype.tick = function () {
     let realDelta = this.delta;
 
-    if (this.typing) {
-        this.current = this.targets[this.currentElement].substring(0, this.current.length + 1);
-    } else {
-        this.current = this.targets[this.currentElement].substring(0, this.current.length - 1);
-    }
-
+    this.current = this.targets[this.index].substring(0, this.typing ? this.current.length + 1 : this.current.length - 1);
     this.element.innerText = this.current;
 
-    if (this.typing && this.current === this.targets[this.currentElement]) {
+    // If phrase is complete wait for a sec for it to be readable
+    if (this.typing && this.current === this.targets[this.index]) {
         this.typing = false;
         realDelta = 1200;
     } else if (!this.typing && this.current === "") {
         this.typing = true;
-        this.currentElement++;
-        if (this.currentElement === this.targets.length) {
-            this.currentElement = 0;
+        this.index++;
+        if (this.index === this.targets.length) {
+            this.index = 0;
         }
+    }
+
+    // Don't wait for next character if last one is a space
+    if (this.current[this.current.length - 1] === " ") {
+        realDelta = 0;
     }
 
     setTimeout(() => {
